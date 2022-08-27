86ec7e618484dbda8ed4dd20b9a3c1ccbd9ff58f
wenhe <wenhelog@gmail.com>
2022-08-27 18:01:27 +0800

    MOD: edit style

--- src/components/AlbumSearch/Album.tsx
+++ src/components/AlbumSearch/Album.tsx
@@ -2,7 +2,9 @@ import { AlbumDetail } from './Album.d';
 
 export const Album = (props: { album: AlbumDetail }) => {
   return (
-    <div className='flex flex-row p-2 border border-rounded'>
+    <div 
+      className='flex flex-row p-2 border rounded-2 border-gray-400 hover-border-black transition-colors cursor-pointer'
+    >
       <img src={props.album.thumb}></img>
       <div className='ml-5 flex flex-col align-center ml-2'>
         <div className='mt-1 text-xl'> {props.album.albumName} </div>
--- src/components/AlbumSearch/AlbumSearch.tsx
+++ src/components/AlbumSearch/AlbumSearch.tsx
@@ -9,33 +9,34 @@ const AlbumSearch = () => {
 
   return (
     <div className="flex flex-col justify-center align-mid">
-      <h1 className="text-center"> Album Search </h1>
-      <input
-        className="mt-2 h-10 border border-gray-400 rounded-4 focus:outline-none focus:border-gray-500"
-        type="text"
-        content={searchContent}
-        onChange={(event) => {
-          setSearchContent(event.target.value);
-        }}
-      />
-      <button
-        className="mx-auto my-2 h-10 w-20 bg-coolGray border-none rounded-full"
-        type="submit"
-        onClick={() => {
-          if (searchContent == "") return;
-          searchAlbum(searchContent).then((res) => {
-            if (res != null) {
-              setAlbums(res);
-            }
-          });
-        }}
-      >
-        Search
-      </button>
-      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2">
-        {albums.length == 0 ? (
-          <div> no albums </div>
-        ) : (
+      <h2 className="pl-1 font-semibold h-6 text-3xl text-left">Music Search </h2>
+      <div className="flex flex-row w-full leading-6 font-medium rounded-2 border border-gray hover-border-black transition-all hover-border-2">
+        <input
+          className="w-full px-4 py-auto text-base border-none outline-none bg-transparent"
+          placeholder="Albums, artists, labels..."
+          type="search"
+          content={searchContent}
+          onChange={(event) => {
+            setSearchContent(event.target.value);
+          }}
+        />
+        <button
+          className="mx-auto h-10 w-20 bg-transparent border-none cursor-pointer op-50 ml-2 hover:op-100 transition-opacity cursor-pointer"
+          type="submit"
+          onClick={() => {
+            if (searchContent == "") return;
+            searchAlbum(searchContent).then((res) => {
+              if (res != null) {
+                setAlbums(res);
+              }
+            });
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="mt-4 grid gap-4 grid-cols-1 sm:grid-cols-2">
+        {albums.length != 0 && (
           albums.map((album, index) => {
             return <Album key={index} album={album} />;
           })
